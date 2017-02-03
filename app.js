d58ae0e7acd6ccfeb0dac95f003cf667f30671df
kwsou <sou.kamweng@gmail.com>
2017-02-03 00:17:14 -0500

    Added notifications at end of job
    Defaulted sound location for now, and a little notification shows up.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var ar = require('./public/js/autoRecorder');
 var log = require('./public/js/log');
 var argv = require('minimist')(process.argv.slice(2));
+var player = require('play-sound')(opts = {});
+var notifier = require('node-notifier');
 
 var config;
 if(argv.config) {
@@ -8,6 +10,18 @@ if(argv.config) {
 }
 
 ar.start(config).then(function() {
-    log.writeLine('Ending process.');
-    process.exit();
+    notifier.notify({
+      'title': 'Automacro',
+      'message': 'Just finished all crafts'
+    });
+    
+    player.play('./public/media/tuturu.mp3', function(err) {
+        notifier.notify({
+            'title': 'Automacro',
+            'message': 'Just finished all crafts'
+        }, function(err, resp) { 
+            log.writeLine('Ending process.');
+            process.exit();
+        });
+    });
 });
